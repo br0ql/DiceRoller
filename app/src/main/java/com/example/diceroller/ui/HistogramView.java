f749724f16cd26b006b475a05f54d49f8018493e
br0 <brooooski8@gmail.com>
2026-02-10 23:59:07 +0100

    Improved histogram UI - text centering, roll numbers inside bars.

--- app/src/main/java/com/example/diceroller/ui/HistogramView.java
+++ app/src/main/java/com/example/diceroller/ui/HistogramView.java
@@ -13,7 +13,8 @@ public class HistogramView extends View {
 
     private Paint barPaint;
     private Paint axisPaint;
-    private Paint textPaint;
+    private Paint labelPaint;
+    private Paint valuePaint;
 
     public HistogramView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -23,15 +24,20 @@ public class HistogramView extends View {
     private void init() {
         barPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         axisPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        textPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-
         barPaint.setColor(Color.parseColor("#4F46E5")); // indigo
         axisPaint.setColor(Color.LTGRAY);
         axisPaint.setStrokeWidth(2f);
 
-        textPaint.setColor(Color.DKGRAY);
-        textPaint.setTextSize(28f);
-        textPaint.setTextAlign(Paint.Align.RIGHT);
+        labelPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        labelPaint.setColor(Color.DKGRAY);
+        labelPaint.setTextSize(28f);
+        labelPaint.setTextAlign(Paint.Align.RIGHT);
+
+        valuePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        valuePaint.setColor(Color.WHITE);
+        valuePaint.setTextSize(26f);
+        valuePaint.setTextAlign(Paint.Align.CENTER);
+
     }
 
     public void setData(int[] counts) {
@@ -63,12 +69,15 @@ public class HistogramView extends View {
         float rowHeight = availableHeight / barCount;
         float startX = paddingLeft;
 
+
         for (int i = 1; i < barCount; i++) {
             float value = counts[i];
             float barLength = (value / maxCount) * (availableWidth * 0.9f);
 
             float top = paddingTop + i * rowHeight;
             float bottom = top + rowHeight * 0.7f;
+            Paint.FontMetrics fm = valuePaint.getFontMetrics();
+            float textY = (top + bottom) / 2 - (fm.ascent + fm.descent) / 2;
 
             float left = startX;
             float right = left + barLength;
@@ -80,17 +89,19 @@ public class HistogramView extends View {
             canvas.drawText(
                     String.valueOf(i),
                     paddingLeft - 16,
-                    bottom - 8,
-                    textPaint
+                    textY,
+                    labelPaint
             );
 
             // wartosc kostki w srodku bara
-            canvas.drawText(
-                    String.valueOf(counts[i]) + " rolls",
-                    paddingLeft + 120,
-                    bottom - 8,
-                    textPaint
-            );
+            if (counts[i] != 0) {
+                canvas.drawText(
+                        String.valueOf(counts[i]) + " rolls",
+                        paddingLeft + 60,
+                        textY,
+                        valuePaint
+                );
+            }
         }
     }
 

2834fefb2d1e9a435d9bd9afec03e779d5b4ba45
br0 <brooooski8@gmail.com>
2026-02-12 23:18:47 +0100

    Initial commit for UI update that will bring working material ui histogram for showing results.

--- app/src/main/java/com/example/diceroller/ui/MainActivity.java
+++ app/src/main/java/com/example/diceroller/ui/MainActivity.java
@@ -25,13 +25,10 @@ public class MainActivity extends AppCompatActivity {
     private HistogramView histogramView;
 
     private TextView stateText;
-    private EditText valueInput;
-    private EditText countInput;
+    private EditText rollCount;
 
-    private Button rerollEqualBtn;
-    private Button rerollAboveBtn;
-    private Button deleteEqualBtn;
-    private Button deleteAboveBtn;
+
+
     private Button undoBtn;
     private Button newRollBtn;
 
@@ -40,57 +37,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        diceRoller = new DiceRoller(new Dice(10));
-//        histogramView = findViewById(R.id.histogramView);
-//
+        diceRoller = new DiceRoller(new Dice(6));
+        histogramView = findViewById(R.id.histogramView);
+
 //        stateText = findViewById(R.id.stateText);
-//        valueInput = findViewById(R.id.valueInput);
-//        countInput = findViewById(R.id.countInput);
-//
-//        rerollEqualBtn = findViewById(R.id.rerollEqualBtn);
-//        rerollAboveBtn = findViewById(R.id.rerollAboveBtn);
-//        deleteEqualBtn = findViewById(R.id.deleteEqualBtn);
-//        deleteAboveBtn = findViewById(R.id.deleteAboveBtn);
-//        undoBtn = findViewById(R.id.undoBtn);
-//        undoBtn.setEnabled(false);
-//        newRollBtn = findViewById(R.id.newRollBtn);
-//
-//        rerollEqualBtn.setOnClickListener(v -> {
-//            diceRoller.rerollEqual(getDiceValue());
-//            renderState();
-//            updateResultsUI();
-//
-//        });
-//
-//        rerollAboveBtn.setOnClickListener(v -> {
-//            diceRoller.rerollAboveOrEqual(getDiceValue());
-//            renderState();
-//            updateResultsUI();
-//        });
-//
-//        deleteEqualBtn.setOnClickListener(v -> {
-//            diceRoller.deleteEqual(getDiceValue());
-//            renderState();
-//            updateResultsUI();
-//        });
-//
-//        deleteAboveBtn.setOnClickListener(v -> {
-//            diceRoller.deleteAboveOrEqual(getDiceValue());
-//            renderState();
-//            updateResultsUI();
-//        });
-//
-//        undoBtn.setOnClickListener(v -> {
-//            diceRoller.undo();
-//            renderState();
-//            updateResultsUI();
-//        });
-//
-//        newRollBtn.setOnClickListener(v -> {
-//            diceRoller.rollMany(getDiceCount());
-//            renderState();
-//            updateResultsUI();
-//        });
+
+        TextView rollCount = findViewById(R.id.rollCountText);
+        int value = Integer.parseInt(rollCount.getText().toString());
+
+        undoBtn = findViewById(R.id.undoBtn);
+        undoBtn.setEnabled(false);
+        newRollBtn = findViewById(R.id.newRollBtn);
+
+        undoBtn.setOnClickListener(v -> {
+            diceRoller.undo();
+            renderState();
+            updateResultsUI();
+        });
+
+        newRollBtn.setOnClickListener(v -> {
+            diceRoller.rollMany(value);
+            renderState();
+            updateResultsUI();
+        });
 
         getWindow().setSoftInputMode(
                 WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN
@@ -103,14 +72,14 @@ public class MainActivity extends AppCompatActivity {
     private void renderState() {
         int[] histogram = diceRoller.getHistogram();
 
-//        if (diceRoller.getRolls().isEmpty()) {
+        if (diceRoller.getRolls().isEmpty()) {
 //            stateText.setText(
 //                    "No rolls yet\n\n" +
 //                            "Use \"New roll\" to start rolling dice."
 //            );
-//            updateUndoState();
-//            return;
-//        }
+            updateUndoState();
+            return;
+        }
 
         StringBuilder sb = new StringBuilder();
         int numberOfRolls = 0;
@@ -127,65 +96,8 @@ public class MainActivity extends AppCompatActivity {
                 .append(numberOfRolls);
 
 //        stateText.setText(sb.toString());
-//        updateUndoState();
+        updateUndoState();
     }
-    private int getDiceValue() {
-        String text = valueInput.getText().toString().trim();
-
-        if (text.isEmpty()) {
-            valueInput.setError("Value required");
-            return MIN_DICE_VALUE;
-        }
-
-        int value;
-        try {
-            value = Integer.parseInt(text);
-        } catch (NumberFormatException e) {
-            valueInput.setError("Invalid number");
-            return MIN_DICE_VALUE;
-        }
-
-        if (value < MIN_DICE_VALUE || value > MAX_DICE_VALUE) {
-            valueInput.setError(
-                    "Value must be between " +
-                            MIN_DICE_VALUE + " and " + MAX_DICE_VALUE
-            );
-            return clamp(value, MIN_DICE_VALUE, MAX_DICE_VALUE);
-        }
-
-        valueInput.setError(null);
-        return value;
-    }
-
-
-    private int getDiceCount() {
-        String text = countInput.getText().toString().trim();
-
-        if (text.isEmpty()) {
-            countInput.setError("Number of dice required");
-            return MIN_DICE_COUNT;
-        }
-
-        int count;
-        try {
-            count = Integer.parseInt(text);
-        } catch (NumberFormatException e) {
-            countInput.setError("Invalid number");
-            return MIN_DICE_COUNT;
-        }
-
-        if (count < MIN_DICE_COUNT || count > MAX_DICE_COUNT) {
-            countInput.setError(
-                    "Number of dice must be between " +
-                            MIN_DICE_COUNT + " and " + MAX_DICE_COUNT
-            );
-            return clamp(count, MIN_DICE_COUNT, MAX_DICE_COUNT);
-        }
-
-        countInput.setError(null);
-        return count;
-    }
-
 
     private void updateUndoState() {
         undoBtn.setEnabled(diceRoller.canUndo());
@@ -197,6 +109,6 @@ public class MainActivity extends AppCompatActivity {
 
     private void updateResultsUI() {
         histogramView.setData(diceRoller.getHistogram());
-        stateText.setText(diceRoller.getRolls().toString());
+//        stateText.setText(diceRoller.getRolls().toString());
     }
 }
